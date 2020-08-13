201f840be30624d8bf260ff287398287da160f56
Lucas Barroso <lucasbarroso23@gmail.com>
2020-08-13 15:12:54 -0300

    desenvolvendo a box da pagina principal

--- routes.js
+++ routes.js
@@ -3,9 +3,16 @@ const routes = express.Router()
 
 
 routes.get("/", function(req, res) {
-    return res.send("ok")
+    return res.redirect("/instructors")
 })
 
+routes.get("/instructors", function(req, res) {
+    return res.render("instructors/index")
+})
+
+routes.get("/members", function(req, res) {
+    return res.send("members")
+})
 
 
 
