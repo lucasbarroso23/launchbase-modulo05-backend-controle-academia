4923f73bb342f3c8976acfb8e00fc8cf3cedf698
Lucas Barroso <lucasbarroso23@gmail.com>
2020-08-15 10:59:59 -0300

    criando paragina para mostrar instrutor pelo id

--- instructors.js
+++ instructors.js
@@ -1,6 +1,27 @@
 const fs = require('fs')
 const data = require('./data.json')
 
+exports.show = function(req, res) {
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor) {
+        return instructor.id == id
+    })
+
+    if(!foundInstructor) return res.send("Instructor not found!")
+
+    const instructor = {
+        ...foundInstructor,
+        age: "",
+        services: foundInstructor.services.split(","),
+        created_at: "",
+    }
+
+    return res.render("instructors/show", {instructor})
+}
+
+
+
 exports.post = function(req, res) {
 
     //estrutura de validação
@@ -12,15 +33,31 @@ exports.post = function(req, res) {
         }
     }
 
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
+    let {avatar_url, birth, name, services, gender} = req.body
 
-    data.instructors.push(req.body)
+    req.body.birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+
+    data.instructors.push({
+        avatar_url,
+        birth,
+        gender,
+        services,
+        created_at,
+        id,
+        name,
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if (err) return res.send("Write file error")
+        if (err) {
+            return res.send("Write file error")
+        }else{
+            return res.redirect("/instructors")
 
-        return res.redirect("/instructors")
+        }
+        
     })
 
     //return res.send(req.body)
