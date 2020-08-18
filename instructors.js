df935f178f3ee6f9cbea303a2276844094da4819
Lucas Barroso <lucasbarroso23@gmail.com>
2020-08-18 15:06:59 -0300

    adicionando a funcionalidade de editar os instrutores

--- instructors.js
+++ instructors.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
-const { age } = require('./utils')
+const { age, date } = require('./utils')
 
 exports.show = function(req, res) {
     const { id } = req.params
@@ -22,8 +22,6 @@ exports.show = function(req, res) {
 
     return res.render("instructors/show", {instructor})
 }
-
-
 
 exports.post = function(req, res) {
 
@@ -56,15 +54,12 @@ exports.post = function(req, res) {
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if (err) return res.send("Write file error")
         
-        return res.redirect("/instructors")
-
-        
+        return res.render("instructors/show", { data })      
         
     })
 
     //return res.send(req.body)
 }
-
 
 exports.edit = function(req, res){
 
@@ -76,5 +71,40 @@ exports.edit = function(req, res){
 
     if(!foundInstructor) return res.send("Instructor not found!")
 
-    return res.render('instructors/edit', { instructor: foundInstructor })
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    date(foundInstructor.birth)
+
+    return res.render('instructors/edit', { instructor })
+}
+
+exports.put = function(req, res) {
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex) {
+        if(id == instructor.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if(!foundInstructor) return res.send("Instructor not found!")
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write error!")
+
+        return res.redirect(`/instructors/${id}`)
+    })
 }
