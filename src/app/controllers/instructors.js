2f466742a305a4683e8c74abb706c4dc1ce9b51d
Lucas Barroso <lucasbarroso23@gmail.com>
2020-08-26 13:18:06 -0300

    fazendo a conexao com o banco de dados

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,4 +1,5 @@
 const { age, date } = require('../../lib/utils')
+const db = require('../../config/db')
 
 
 module.exports = {
@@ -16,10 +17,35 @@ module.exports = {
                 return res.send('Please fill all fields!')
             }
         }
+
+        const query = `
+            INSERT INTO instructors (
+                name, 
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING id
+        `
+
+        const values = [
+            req.body.name,
+            req.body.avatar_url,
+            req.body.gender,
+            req.body.services,
+            date(req.body.birth).iso,
+            date(Date.now()).iso
+        ]
+
+        db.query(query, values, function(err, results) {
+            if(err) return res.send("Database Error!")
+
+            return res.redirect(`/instructors/${results.rows[0].id}`)
+        })
     
-        let {avatar_url, birth, name, services, gender} = req.body
-    
-        return
+        
         
     },
     show(req, res) {
