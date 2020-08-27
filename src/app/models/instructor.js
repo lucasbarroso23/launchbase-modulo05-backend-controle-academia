fe79c0e8c0c6af8760b00beee584f27c8c1d39ec
Lucas Barroso <lucasbarroso23@gmail.com>
2020-08-27 11:59:04 -0300

    Concluida a configuraçao de instrutores da aplicaçao

--- src/app/models/instructor.js
+++ src/app/models/instructor.js
@@ -1,12 +1,12 @@
 const { date } = require('../../lib/utils')
 
 const db = require('../../config/db')
-const instructors = require('../controllers/instructors')
+
 
 module.exports = {
     all(callback) {
         db.query(`SELECT * FROM instructors`, function(err, results){
-            if(err) return res.send("Database Error!")
+            if(err)  throw `Database Error! ${err}`
 
         callback(results.rows)
         })
@@ -34,7 +34,7 @@ module.exports = {
         ]
 
         db.query(query, values, function(err, results) {
-            if(err) return res.send("Database Error!")
+            if(err) throw `Database Error! ${err}`
 
             callback(results.rows[0])
         })
@@ -44,9 +44,44 @@ module.exports = {
             SELECT * 
             FROM instructors 
             WHERE id = $1`, [id], function(err, results){
-                if(err) return res.send("Database Error!")
+                if(err) throw `Database Error! ${err}`
 
                 callback(results.rows[0])
+        })
+    },
+    update(data, callback) {
+        const query = `
+        UPDATE instructors SET
+            avatar_url=($1),
+            name=($2),
+            birth=($3),
+            gender=($4),
+            services=($5)
+        WHERE id = $6
+        `
+
+        const values = [
+            data.avatar_url,
+            data.name,
+            date(data.birth).iso,
+            data.gender,
+            data.services,
+            data.id
+        ]
+
+        db.query(query, values, function(err, results){
+            if(err)  throw `Database Error! ${err}`
+
+            callback()
+        })
+    },
+    delete(id, callback) {
+        db.query(`
+        DELETE FROM instructors 
+        WHERE id = $1`, [id], function(err, results){
+            if(err)  throw `Database Error! ${err}`
+
+            callback()
         })
     }
 }
