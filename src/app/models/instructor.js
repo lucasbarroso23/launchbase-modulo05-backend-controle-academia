ea7d36fe3f2dcf2fe41c035a7a937c4dfde4044b
Lucas Barroso <lucasbarroso23@gmail.com>
2020-08-30 22:47:08 -0300

    começando a logica de paginaçao do sistema

--- src/app/models/instructor.js
+++ src/app/models/instructor.js
@@ -1,6 +1,7 @@
 const { date } = require('../../lib/utils')
 
 const db = require('../../config/db')
+const { off } = require('../../config/db')
 
 
 module.exports = {
@@ -104,5 +105,32 @@ module.exports = {
 
             callback()
         })
+    },
+    paginate(params) {
+        const { filter, limit, offset, callback } = params
+
+        let query = `
+        SELECT instructors.*, count(members) as total_students 
+        FROM instructors
+        LEFT JOIN members ON (instructors.id = members.instructor_id)
+        `
+
+        if( filter ) {
+            query = `${query}
+            WHERE instructors.name ILIKE '%${filter}%'
+            OR instructors.services ILIKE '%${filter}%'
+            `
+        }
+
+        query = `${query}
+        GROUP BY instructors.id LIMIT $1 OFFSET $2
+        `
+        db.query(query, [limit, offset], function(err, results) {
+            if (err) throw `Database Error! ${err}`
+
+            callback(results.rows)
+
+        })
+
     }
 }
